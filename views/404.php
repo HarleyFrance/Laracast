a773c79a1a35122d5f300e5f31bfcedabcbbd225
Harley France <severfrancedark@gmail.com>
2023-10-07 21:51:06 +0800

    Make php router

---
+++ views/404.php
@@ -0,0 +1,14 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+
+<main>
+     <div class="flex items-center justify-center min-h-screen">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold mb-4">Sorry. Page not Found.</h1>
+            <a href="/" class="text-blue-500 hover:underline">Go back home</a>
+        </div>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
